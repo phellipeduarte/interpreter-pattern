c055a907959944d201dcf1a89620e3c0ad3f6157
phellipeduarte <phellipe.duarte@icloud.com>
2023-06-09 20:20:21 -0300

    Adicionando classes, testes e diagramas

---
+++ src/main/java/Hospede.java
@@ -0,0 +1,30 @@
+public class Hospede {
+
+    private Integer tempo;
+    private Integer quarto;
+
+    public Hospede(Integer tempo, Integer quarto) {
+        this.tempo = tempo;
+        this.quarto = quarto;
+    }
+
+    public Integer getTempo() {
+        return tempo;
+    }
+
+    public void setTempo(Integer tempo) {
+        this.tempo = tempo;
+    }
+
+    public Integer getQuarto() {
+        return quarto;
+    }
+
+    public void setQuarto(Integer quarto) {
+        this.quarto = quarto;
+    }
+
+    public Double consultarValorEstadia(){
+        return Hotel.calcularEstadia(this);
+    }
+}
---
+++ src/test/java/HospedeTest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class HospedeTest {
+
+    @Test
+    void deveCalcularEstadia(){
+        Hospede hospede = new Hospede(30, 505);
+        assertEquals(1750.0, hospede.consultarValorEstadia());
+    }
+}
